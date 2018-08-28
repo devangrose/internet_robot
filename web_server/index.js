12111de996fe02a1166e83ddd337e5bc16dc5cfe
Devan Grose <devangrose@gmail.com>
2018-08-28 12:31:47 -0700

    added procfile and a few changes

--- web_server/index.js
+++ web_server/index.js
@@ -33,6 +33,4 @@ app.get('/commands', function(req, res) {
 });
 
 // listen on port 3000
-app.listen(3000, function (){
-    console.log('listening on port 3000');
-});
+app.listen(process.env.PORT || 3000);

21184aad27a42111d9956b2fcc51d362fc9cb478
Devan Grose <devangrose@gmail.com>
2018-08-31 10:58:35 -0700

    limited number of commands and made last clicked arrow red

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -1,47 +1,63 @@
 
 $(document).ready(function () {
 
+    var numberOfCommands = 0;
     // Attach listeners to arrows
     $('#arrow-up').click(function (e) {
         clearClasses();
-        $('#arrow-up img').attr('src','/img/arrow-up.png');
-        var newImage = $('<img>');
-        newImage.attr('src','/img/arrow-up.png');
-        newImage.addClass('queue-image');
-        $('#queue').append(newImage);
-        var commString = $('#command-string').val();
-        commString += ' forward';
-        $('#command-string').val(commString);
+        numberOfCommands++;
+        $('#arrow-up img').attr('src','/img/arrow-up-red.png');
+        if(numberOfCommands < 10) {
+            var newImage = $('<img>');
+            newImage.attr('src','/img/arrow-up.png');
+            newImage.addClass('queue-image');
+            $('#queue').append(newImage);
+            var commString = $('#command-string').val();
+            commString += ' forward';
+            $('#command-string').val(commString);
+        }
     });
     $('#arrow-down').click(function (xe) {
         clearClasses();
-        var newImage = $('<img>');
-        newImage.attr('src','/img/arrow-down.png');
-        newImage.addClass('queue-image');
-        $('#queue').append(newImage);
-        var commString = $('#command-string').val();
-        commString += ' backward';
-        $('#command-string').val(commString);
+        numberOfCommands++;
+        $('#arrow-down img').attr('src','/img/arrow-down-red.png');
+        if(numberOfCommands < 10) {
+            var newImage = $('<img>');
+            newImage.attr('src','/img/arrow-down.png');
+            newImage.addClass('queue-image');
+            $('#queue').append(newImage);
+            var commString = $('#command-string').val();
+            commString += ' backward';
+            $('#command-string').val(commString);
+        }
     });
     $('#arrow-left').click(function (xe) {
         clearClasses();
-        var newImage = $('<img>');
-        newImage.attr('src','/img/arrow-left.png');
-        newImage.addClass('queue-image');
-        $('#queue').append(newImage);
-        var commString = $('#command-string').val();
-        commString += ' left';
-        $('#command-string').val(commString);
+        numberOfCommands++;
+        $('#arrow-left img').attr('src','/img/arrow-left-red.png');
+        if(numberOfCommands < 10) {
+            var newImage = $('<img>');
+            newImage.attr('src','/img/arrow-left.png');
+            newImage.addClass('queue-image');
+            $('#queue').append(newImage);
+            var commString = $('#command-string').val();
+            commString += ' left';
+            $('#command-string').val(commString);
+        }
     });
     $('#arrow-right').click(function (xe) {
         clearClasses();
-        var newImage = $('<img>');
-        newImage.attr('src','/img/arrow-right.png');
-        newImage.addClass('queue-image');
-        $('#queue').append(newImage);
-        var commString = $('#command-string').val();
-        commString += ' right';
-        $('#command-string').val(commString);
+        numberOfCommands++;
+        $('#arrow-right img').attr('src','/img/arrow-right-red.png');
+        if(numberOfCommands < 10) {
+            var newImage = $('<img>');
+            newImage.attr('src','/img/arrow-right.png');
+            newImage.addClass('queue-image');
+            $('#queue').append(newImage);
+            var commString = $('#command-string').val();
+            commString += ' right';
+            $('#command-string').val(commString);
+        }
     });
 });
 
